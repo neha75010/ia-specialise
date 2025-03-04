cf3e352b0e82a704de70c6313c6725b56c1c4231
neha75010 <73582439+neha75010@users.noreply.github.com>
2025-03-04 12:10:24 +0100

    update

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import cv2
-# Charger une image
+# charger l'image
 image = cv2.imread('./image.jpg')
 # Afficher l'image
-cv2.imshow('Image', image) cv2.waitKey(0) cv2.destroyAllWindows()
+cv2.imshow('Image', image) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
 
 # Convertir en niveaux de gris
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Sauvegarder
 cv2.imwrite('gray_image.jpg', gray_image)
 # Afficher l'image
-cv2.imshow('Grayscale Image', gray_image) cv2.waitKey(0)
+cv2.imshow('Grayscale Image', gray_image) 
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Redimensionner à une taille fixe
@@ -17,9 +20,11 @@ resized_image = cv2.resize(image, (200, 200))
 # Redimensionner en maintenant le ratio
 height, width = image.shape[:2]
 new_width = 300
-new_height = int((new_width / width) * height) resized_aspect_image = cv2.resize(image, (new_width, new_height))
+new_height = int((new_width / width) * height) 
+resized_aspect_image = cv2.resize(image, (new_width, new_height))
 # Afficher l'image
-cv2.imshow('Resized Image', resized_image) cv2.waitKey(0)
+cv2.imshow('Resized Image', resized_image) 
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # Normalisation entre 0 et 1
@@ -27,5 +32,6 @@ normalized_image = image / 255.0
 # Normalisation à une plage personnalisée
 norm_image = cv2.normalize(image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
 # Afficher l'image
-cv2.imshow('Normalized Image', norm_image) cv2.waitKey(0)
+cv2.imshow('Normalized Image', norm_image) 
+cv2.waitKey(0)
 cv2.destroyAllWindows()
