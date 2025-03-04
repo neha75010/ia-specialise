e5bf14878a45362b56dcb57681b806999c2d8a8b
neha75010 <73582439+neha75010@users.noreply.github.com>
2025-03-04 12:32:56 +0100

    suite tp1

--- app.py
+++ app.py
@@ -18,3 +18,11 @@ def model_predict(img, model):
    x = preprocess_input(x, mode='tf')
    preds = model.predict(x)
    return preds
+
+# prediction
+preds = model_predict(img, model)
+pred_proba = "{:.3f}".format(np.amax(preds))
+pred_class = decode_predictions(preds, top=1)
+result = str(pred_class[0][0][1])
+result = result.replace('_', ' ').capitalize()
+print({"result":result, "probability":pred_proba})
